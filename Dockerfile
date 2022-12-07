4b213d0f09514d8a78a2dbb3879704d58f5267f9
Min Park <plan@quantec.co.kr>
2022-12-07 09:52:37 +0900

    Updated

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,11 @@
 FROM apache/airflow:latest-python3.9
+USER root
+RUN apt-get update \
+  && apt-get install -y --no-install-recommends \
+         git \
+  && apt-get autoremove -yqq --purge \
+  && apt-get clean \
+  && rm -rf /var/lib/apt/lists/*
+USER airflow
 COPY requirements.txt .
 RUN pip install -r requirements.txt
--- dags/analyzer.py
+++ dags/analyzer.py
@@ -2,12 +2,16 @@ from airflow import DAG
 from airflow.decorators import task
 from airflow.operators.bash import BashOperator
 
+from git import Repo, Git
+import os
 import nltk
-from pendulum import datetime
+
+from datetime import datetime, date
+from dateutil import tz
 
 with DAG(
     dag_id="analyzer",
-    start_date=datetime(2022,12,5,tz='Asia/Seoul'),
+    start_date=datetime(2022,12,5,tzinfo=tz.gettz('Asia/Seoul')),
     schedule="40 16 * * 3") as dag:
 
     @task
@@ -22,4 +26,27 @@ with DAG(
     run_analyzer_us = BashOperator(task_id='run_analyzer_us', bash_command=analyzer_us_str, dag=dag)
     run_analyzer_kr = BashOperator(task_id='run_analyzer_kr', bash_command=analyzer_kr_str, dag=dag)
 
-    preload() >> run_analyzer_us >> run_analyzer_kr
+    @task
+    def git_sync():
+        base_dir = '/opt/devs/Q_SA_Int/'
+        data_dir = base_dir + 'data/current/'
+
+        # Files to commit
+        file_paths = list(map(lambda x:os.path.join(data_dir,x.name),os.scandir(data_dir)))
+        print(file_paths)
+
+        # Initialize GitHub
+        repo = Repo(base_dir)
+
+        # Commit msg
+        msg = f"Analyzed data as of {date.today().strftime('%Y-%m-%d')} Uploaded"
+        # Stage files
+        repo.index.add(file_paths)
+        # Commit files
+        repo.index.commit(msg)
+
+        # Pull & Push
+        repo.remotes.origin.pull()
+        repo.remotes.origin.push()
+
+    preload() >> run_analyzer_us >> run_analyzer_kr >> git_sync()
