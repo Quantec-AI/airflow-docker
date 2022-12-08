1c6afa626f671eedf6c8e2e85e67c465920ec072
Min Park <plan@quantec.co.kr>
2022-12-08 17:30:40 +0900

    Operator within DAGs

--- dags/analyzer.py
+++ dags/analyzer.py
@@ -11,59 +11,60 @@ from dateutil import tz
 
 from ms_teams_webhook_operator import MSTeamsWebhookOperator
 
-@task
-def preload():
-    nltk.download('punkt')
-
 items = {'US':['US','Topics'],'KR':['KR','Equity']}
 
 analyzer_us_str = f"cd /opt/devs/Q_SA_Int && python analyzers/news_analyzer.py --region={items.get('US')[0]} --product_type={items.get('US')[1]}"
 analyzer_kr_str = f"cd /opt/devs/Q_SA_Int && python analyzers/news_analyzer.py --region={items.get('KR')[0]} --product_type={items.get('KR')[1]}"
 
-run_analyzer_us = BashOperator(task_id='run_analyzer_us', bash_command=analyzer_us_str)
-run_analyzer_kr = BashOperator(task_id='run_analyzer_kr', bash_command=analyzer_kr_str)
-
-@task
-def git_sync():
-    base_dir = '/opt/devs/Q_SA_Int/'
-    data_dir = base_dir + 'data/current/'
-
-    # Files to commit
-    file_paths = list(map(lambda x:os.path.join(data_dir,x.name),os.scandir(data_dir)))
-    print(file_paths)
-
-    # Initialize GitHub
-    repo = Repo(base_dir)
-
-    # Commit msg
-    msg = f"Analyzed data as of {date.today().strftime('%Y-%m-%d')} Uploaded"
-
-    try:
-        # Stage files
-        repo.index.add(file_paths)
-        # Commit files
-        repo.index.commit(msg)
-
-        # Pull & Push
-        repo.remotes.origin.pull()
-        repo.remotes.origin.push()
-
-    except:
-        msg = 'Git Sync Failed'
-
-    return msg
-
-teams_msg = MSTeamsWebhookOperator(task_id='ms_teams_msg',
-    http_conn_id='msteams_webhook_url',
-    message = "Git Sync Result",
-    subtitle = "{{ ti.xcom_pull(task_ids='git_sync') }}",
-    theme_color = "00FF00",
-    button_text = "GitHub",
-    button_url = "https://github.com/Quantec-AI/Q_SA_Int")
-
 with DAG(
     dag_id="analyzer",
     start_date=datetime(2022,11,30,tzinfo=tz.gettz('Asia/Seoul')),
     schedule="40 16 * * 3") as dag:
+
+    @task
+    def preload():
+        nltk.download('punkt')
+
+    run_analyzer_us = BashOperator(task_id='run_analyzer_us', bash_command=analyzer_us_str)
+    run_analyzer_kr = BashOperator(task_id='run_analyzer_kr', bash_command=analyzer_kr_str)
+
+    @task
+    def git_sync():
+        base_dir = '/opt/devs/Q_SA_Int/'
+        data_dir = base_dir + 'data/current/'
+
+        # Files to commit
+        file_paths = list(map(lambda x:os.path.join(data_dir,x.name),os.scandir(data_dir)))
+        print(file_paths)
+
+        # Initialize GitHub
+        repo = Repo(base_dir)
+
+        # Commit msg
+        msg = f"Analyzed data as of {date.today().strftime('%Y-%m-%d')} Uploaded"
+
+        try:
+            # Stage files
+            repo.index.add(file_paths)
+            # Commit files
+            repo.index.commit(msg)
+
+            # Pull & Push
+            repo.remotes.origin.pull()
+            repo.remotes.origin.push()
+
+        except:
+            msg = 'Git Sync Failed'
+
+        return msg
+
+    teams_msg = MSTeamsWebhookOperator(task_id='ms_teams_msg',
+        http_conn_id='msteams_webhook_url',
+        message = "Git Sync Result",
+        subtitle = "{{ ti.xcom_pull(task_ids='git_sync') }}",
+        theme_color = "00FF00",
+        button_text = "GitHub",
+        button_url = "https://github.com/Quantec-AI/Q_SA_Int")
+
 
     preload() >> run_analyzer_us >> run_analyzer_kr >> git_sync() >> teams_msg
--- dags/scrapy_us_equity.py
+++ dags/scrapy_us_equity.py
@@ -7,7 +7,6 @@ from pendulum import datetime
 from scrapy_intervals import USEquityScrapyTimetable
 
 scrapy_str = f'cd /opt/devs/Q_SA_Int && scrapy crawl Q_SA -a region=US -a product_type=Equity -a lookback_start=10'
-run_scrapy = BashOperator(task_id='run_scrapy', bash_command=scrapy_str)
 
 with DAG(
     dag_id="scrapy_us_equity",
@@ -15,4 +14,6 @@ with DAG(
     timetable=USEquityScrapyTimetable(),
     catchup=False) as dag:
     
+    run_scrapy = BashOperator(task_id='run_scrapy', bash_command=scrapy_str)
+
     run_scrapy
